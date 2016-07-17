9bed60d9a0f9a078cc1f15da7a4fb21815787546
Jack Sorrell <jack@jacksorrell.com>
2016-07-17 05:01:35 -0400

    Remove globals from event page

--- eventPage.js
+++ eventPage.js
@@ -1,47 +1,42 @@
-var token = null;
 
-var tabID = null;
-
-//Authorize
-chrome.identity.getAuthToken({
-    interactive: true
-}, function(tkn) {
-    if (chrome.runtime.lastError) {
-        console.log(chrome.runtime.lastError.message);
-        return;
-    }
-
-    token = tkn;
-});
+function getAuthToken(cb) {
+    chrome.identity.getAuthToken({
+        interactive: true
+    }, function(tkn) {
+        if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError.message);
+            return;
+        } else {
+            cb(tkn);
+        }
+    });
+}
 
 function makePlaylist(openerTab, title, desc, videoIDs) {
-    if (!token) {
-        console.log("No access token");
-        return;
-    }
-
-    $.ajax({
-        "url": "https://www.googleapis.com/youtube/v3/playlists?alt=json&part=snippet&access_token=" + token,
-        "method": "POST",
-        "contentType": "application/json",
-        "data": JSON.stringify({
-            "snippet": {
-                "title": title,
-                "description": desc,
-                "privacyStatus": "private"
+    getAuthToken(function (token) {
+        $.ajax({
+            "url": "https://www.googleapis.com/youtube/v3/playlists?alt=json&part=snippet&access_token=" + token,
+            "method": "POST",
+            "contentType": "application/json",
+            "data": JSON.stringify({
+                "snippet": {
+                    "title": title,
+                    "description": desc,
+                    "privacyStatus": "private"
+                }
+            }),
+            success: function(data, textStatus, jqXHR) {
+                insertVideos(openerTab, data.id, videoIDs);
+                var setData = {};
+                setData[data.id] = {
+                    videos: []
+                }
+                chrome.storage.local.set(setData);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown, jqXHR.responseText);
             }
-        }),
-        success: function(data, textStatus, jqXHR) {
-            insertVideos(openerTab, data.id, videoIDs);
-            var setData = {};
-            setData[data.id] = {
-                videos: []
-            }
-            chrome.storage.local.set(setData);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown, jqXHR.responseText);
-        }
+        });
     });
 }
 
@@ -51,52 +46,54 @@ function insertVideos(openerTab, playlistID, videos) {
     }
 
     var video = videos.pop();
+    getAuthToken(function (token) {
 
-    $.ajax({
-        "url": "https://www.googleapis.com/youtube/v3/playlistItems?alt=json&part=snippet&access_token=" + token,
-        "method": "POST",
-        "contentType": "application/json",
-        "data": JSON.stringify({
-            "snippet": {
-                "playlistId": playlistID,
-                "resourceId": {
-                    "kind": "youtube#video",
-                    "videoId": video.id
-                }
-            }
-        }),
-        success: function(data, textStatus, jqXHR) {
-            chrome.storage.local.get(playlistID,
-                function (res) {
-                    var playlistData = res[playlistID];
-                    if (!playlistData) {
-                        return; //Playlist has already been deleted
+        $.ajax({
+            "url": "https://www.googleapis.com/youtube/v3/playlistItems?alt=json&part=snippet&access_token=" + token,
+            "method": "POST",
+            "contentType": "application/json",
+            "data": JSON.stringify({
+                "snippet": {
+                    "playlistId": playlistID,
+                    "resourceId": {
+                        "kind": "youtube#video",
+                        "videoId": video.id
                     }
-                    video.index = playlistData.videos.length;
-                    playlistData.videos.push(video);
-                    chrome.storage.local.set(res, function () {
-                        if (playlistData.videos.length === 1)
-                            chrome.tabs.create(
-                                {
-                                    url: "https://www.youtube.com/watch?v=" + video.id + "&list=" + playlistID,
-                                    windowId: openerTab.windowId,
-                                    index: openerTab.index+1,
-                                    openerTabId: openerTab.id
-                                },
-                                function (tab) { tabCreatedCallback(tab, playlistID); }
-                            );
-
-                        if (videos.length > 0) {
-                            insertVideos(openerTab, playlistID, videos);
-                        } else {
-                            console.log("done creating playlist");
+                }
+            }),
+            success: function(data, textStatus, jqXHR) {
+                chrome.storage.local.get(playlistID,
+                    function (res) {
+                        var playlistData = res[playlistID];
+                        if (!playlistData) {
+                            return; //Playlist has already been deleted
                         }
+                        video.index = playlistData.videos.length;
+                        playlistData.videos.push(video);
+                        chrome.storage.local.set(res, function () {
+                            if (playlistData.videos.length === 1)
+                                chrome.tabs.create(
+                                    {
+                                        url: "https://www.youtube.com/watch?v=" + video.id + "&list=" + playlistID,
+                                        windowId: openerTab.windowId,
+                                        index: openerTab.index+1,
+                                        openerTabId: openerTab.id
+                                    },
+                                    function (tab) { tabCreatedCallback(tab, playlistID); }
+                                );
+
+                            if (videos.length > 0) {
+                                insertVideos(openerTab, playlistID, videos);
+                            } else {
+                                console.log("done creating playlist");
+                            }
+                        });
                     });
-                });
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown, jqXHR.responseText);
-        }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown, jqXHR.responseText);
+            }
+        });
     });
 }
 
@@ -147,15 +144,17 @@ function processUnwatched(tab, unwatched) {
 }
 
 function deletePlaylist(playlistID) {
-    $.ajax({
-        "url": "https://www.googleapis.com/youtube/v3/playlists?access_token=" + token + "&id=" + playlistID,
-        "method": "DELETE",
-        success: function(data, textStatus, jqXHR) {
-            console.log(playlistID, "deleted");
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown, jqXHR.responseText);
-        }
+    getAuthToken(function (token) {
+        $.ajax({
+            "url": "https://www.googleapis.com/youtube/v3/playlists?access_token=" + token + "&id=" + playlistID,
+            "method": "DELETE",
+            success: function(data, textStatus, jqXHR) {
+                console.log(playlistID, "deleted");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown, jqXHR.responseText);
+            }
+        });
     });
 }
 
